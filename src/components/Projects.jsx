27dbaf626ca252e750b0a88690b07d1ca62b26fb
DmitriyAngve <kirirch129@gmail.com>
2023-10-15 15:53:41 +0700

    #

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import BlogAI from "../assets/BlogAI.png";
-import Dashboard from "../assets/Dashboard.png";
 
+import BlogAI from "../assets/BlogAI.png";
+
+import Dashboard from "../assets/Dashboard.png";
 import DiscordClone from "../assets/Discord-Clone.png";
 import LMSproject from "../assets/LMS-project.png";
 import ThreeJs from "../assets/ThreeJs.png";
